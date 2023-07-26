bb2cf934f6fe488ada1d6cc468c8c3d55072434c
Lena Munad <munadlena@gmail.com>
2023-07-25 22:30:51 -0700

    Added front-end setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
+import * as api from './services/api';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComp/>
-    </div>
-  );
+const [authenticated, setAuthenticated] = useState(false);
+const [username, setUsername] = useState();
+const [password, setPassword] = useState();
+
+const authUser = async () => {
+   setAuthenticated(await api.authenticate(username, password));
 }
 
-export default App;
+const createUser = async () => {
+    await api.createUser(username, password);
+}
+
+return (
+    <div className="App">
+        {!authenticated ? (
+            <div>
+                <label>Username: </label>
+                <br />
+                <input
+                    type="text"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <br />
+                <label>Password: </label>
+                <br />
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <button onClick={createUser}>Create User</button>
+                <button onClick={authUser}>Login</button>
+            </div>
+        ) : (
+            <div className="App">
+                <NavbarComp />
+            </div>
+        )}
+    </div>
+);
